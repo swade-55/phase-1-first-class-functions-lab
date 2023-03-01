87544179074bc64c2ee118f0e4729522d485af3d
SamuelWade <samuelrcwade@gmail.com>
2023-03-01 07:38:27 -0500

    Test Successful

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+let drivers = ['Amari','Mo','Sam'];
+
+const returnFirstTwoDrivers = function(drivers) {
+    let newDrivers = [];
+    for (let index = 0; index < 2; index++) {
+        newDrivers.push(drivers[index]);
+    }
+    return newDrivers;
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);}
+
+let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(x) {
+    return function(y) {return x*y};
+
+}
+
+let fareDoubler = createFareMultiplier(2);
+
+
+let fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers,referenceFunc) {
+    return referenceFunc(drivers);
+}
+
+selectDifferentDrivers(drivers,returnLastTwoDrivers);
+
+selectDifferentDrivers(drivers,returnFirstTwoDrivers);
+
